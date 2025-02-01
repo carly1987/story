8d6517482b2dcf284dc82977398b1725a0e5d78b
526450935@qq.com <526450935@qq.com>
2025-02-01 13:26:09 +0800

    test

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
+            child_thread(app);
             Ok(())
         })
         .plugin(tauri_plugin_persisted_scope::init())
@@ -18,4 +19,13 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn child_thread(app: &mut App) {
+    // 数据库文件的相对程序路径
+    let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // 转化为绝对路径
+    let resolver = app.path();
+    let resource_dir = &resolver.resource_dir().unwrap();
+    let resolver_path = resource_dir.join(&database_url);
 }
