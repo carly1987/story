cab6a8b5c00f23cbfb33952d705fcbf9668e96c7
526450935@qq.com <526450935@qq.com>
2025-02-01 10:40:16 +0800

    test

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,61 +1,6 @@
-mod sqlite_db;
-use crate::sqlite_db::{chapter, content, story, DbManager};
+
 use tauri::{App, Manager};
 
-#[tauri::command]
-fn get_all_story() -> Vec<story::Story> {
-    story::query()
-}
-
-#[tauri::command]
-fn add_story(name: String) -> i32 {
-    story::add(&name)
-}
-
-#[tauri::command]
-fn upload_txt(path: String) {
-    story::upload(path);
-}
-
-#[tauri::command]
-fn open_txt(path: String) -> story::StoryData {
-    story::open(path)
-}
-
-#[tauri::command]
-fn add_chapter(name: &str, story: i32) -> i32 {
-    chapter::add(name, story)
-}
-
-#[tauri::command]
-fn get_all_chapter(id: i32) -> Vec<chapter::Chapter> {
-    chapter::query(id)
-}
-
-#[tauri::command]
-fn get_all_content(chapter: i32, story: i32) -> Vec<content::Content> {
-    content::query(chapter, story)
-}
-
-#[tauri::command]
-fn create_content(content: String, story: i32, chapter: i32) -> i32 {
-    content::create(&content, story, chapter)
-}
-
-#[tauri::command]
-fn update_content(id: String, text: String) -> usize {
-    content::update(id, text)
-}
-
-#[tauri::command]
-fn delete_content(id: i32) -> usize {
-    content::delete(id)
-}
-
-#[tauri::command]
-fn test() -> String {
-    String::from("value")
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -70,17 +15,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
-            test,
-            upload_txt,
-            open_txt,
-            get_all_story,
-            add_story,
-            add_chapter,
-            get_all_chapter,
-            get_all_content,
-            create_content,
-            update_content,
-            delete_content
+            
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
